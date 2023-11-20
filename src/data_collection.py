2adee6e2469c9b4652ca4924ae6567eb2de02320
DataSpi <ptk23122000@gmail.com>
2023-11-20 17:11:14 +0700

    200 data jobs 20/11

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,3 +1,7 @@
+"""
+this scraper still work at 20/11/2023, but i doubt that it will
+continue to work w/o modifying in the future
+"""
 import scraper as scp
 
 # look at this for reference to pass data in the arguments of get_search_soup() right. 
@@ -6,23 +10,34 @@ sample_urls=[
     'https://careerbuilder.vn/viec-lam/van-chuyen-giao-nhan-kho-van-c33-vi.html', # industry Vận chuyển / Giao nhận / Kho vận
     'https://careerbuilder.vn/viec-lam/Data-Analyst-k-vi.html', # keyword Data-Analyst
     'https://careerbuilder.vn/viec-lam/cntt-phan-mem-c1-vi.html', # industry CNTT - Phần mềm
+    'https://careerbuilder.vn/viec-lam/data-k-vi.html', # keyword data
+    'https://careerbuilder.vn/viec-lam/du-lieu-k-vi.html', # keyword du-lieu
+    
 ]
 
 # get search_soup
 search_soups = scp.get_search_soups(
-    key_word='Data-Analyst',
+    key_word='data',
     category_code='k',
-    page_num=4,
+    page_num=20,
     load_sleep_time=10,
     scroll_sleep_time=1
 )
 # Parsing data from the search_pages
 df_search_page = scp.extract_search_page(search_soups=search_soups)
 
+
+"""
+for index, title in enumerate(df_search_page.job_title.tolist()): 
+    print(index, title)
+# after printing out the job-titles, i see that 200 is kinda the sweet spot for relevance, 
+# all the jobs after the 4th page does not seem relevant to the keyword anymore. 
+"""
+
 # Scraping & parsing data from the job_links
 df_job_link = scp.extract_job_links(
-    df_search_page=df_search_page,
-    load_sleep_time=10,
+    df_search_page=df_search_page[:200],
+    load_sleep_time=8,
     scroll_sleep_time=1
 )
 
@@ -36,4 +51,4 @@ final_df = scp.merge_search_page_n_job_link(
 )
 
 # Saving
-# final_df.to_csv('../data/raw/20231911-data-analyst.csv', index=False)
+final_df.to_csv('../data/raw/data-jobs/20232011-data-jobs.csv', index=False)
